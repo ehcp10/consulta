3383fa133bda0b81c12a10345e2ab91464aaa04b
Evandro <evandro.coutodepaula@gmail.com>
2023-09-19 21:21:55 -0300

    Ajuste de modelo

--- core/domain/client/client.go
+++ core/domain/client/client.go
@@ -3,11 +3,51 @@ package client
 import "time"
 
 type Client struct {
-	ID            int       `json:"id,omitempty"`
-	Name          string    `json:"nome"`
-	BirthDate     time.Time `json:"data_de_nascimento"`
-	Occupation    string    `json:"profissão"`
-	Gender        string    `json:"sexo"`
-	Age           uint      `json:"idade"`
-	MaritalStatus string    `json:"estado_civil"`
+	ID            int           `json:"id,omitempty"`
+	Name          string        `json:"nome"`
+	BirthDate     time.Time     `json:"data_de_nascimento"`
+	Occupation    Occupation    `json:"profissão"`
+	Gender        Gender        `json:"sexo"`
+	Age           uint          `json:"idade"`
+	MaritalStatus MaritalStatus `json:"estado_civil"`
+}
+
+type Occupation struct {
+	ID         int
+	Occupation string
+}
+
+type Gender struct {
+	ID     int
+	Gender string
+}
+
+type MaritalStatus struct {
+	ID            int
+	MaritalStatus string
+}
+
+type ContactType struct {
+	ID          int
+	Description string
+}
+
+type Contact struct {
+	ID          int
+	Client      Client
+	ContactType ContactType
+	Value       string
+}
+
+type EmergenceContact struct {
+	ID          int
+	Client      Client
+	ContactType ContactType
+	Value       string
+}
+
+type ClientCost struct {
+	ID          int
+	Client      Client
+	SessionCost float64
 }
